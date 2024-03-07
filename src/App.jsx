7d3c65c6556080fb63790f4ea51e2d370ccbbdec
Rohan Naik <rohannaik@Rohans-MacBook-Air.local>
2024-03-07 12:59:50 +0530

    rohan

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [allEntry, setAllEntry] = useState([])
+
+  function submitForm(e) {
+    e.preventDefault();
+    const newEntry = { email: email, password: password }
+    setAllEntry(...allEntry,newEntry)
+    
+  }
+
+  return (
+    <>
+      <form action="" onSubmit={submitForm}>
+        <div className="container">
+          <div className="email">
+            <h3>email</h3>
+            <label htmlFor="email"></label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="enter email"
+              autoComplete="off"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="password">
+            <h3>password</h3>
+            <label htmlFor="password"></label>
+            <input
+              type="text"
+              id="password"
+              name="password"
+              placeholder="enter password"
+              autoComplete="off"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className="btn">submit</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default App
